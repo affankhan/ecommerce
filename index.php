8125d156e2cf185f8ef7dc5c0c64c263d9be16c0
Affan Khan <affan.ned@gmail.com>
2015-10-02 09:12:55 -0700

    initial commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<title></title>
+		<!--adding the stylesheets and bootstrap-->
+ 	
+ 	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+ 	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+ 	<link rel="stylesheet" type="text/css" href="css/font-awesome.css">
+ 	<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+
+ 	<!--adding custom stylesheets -->
+ 	<link rel="stylesheet" type="text/css" href="css/custom.css">
+ 	
+ 	<!--adding jquery scripts--> 
+ 	<script type="text/javascript" src="js/jquery.js"></script>
+
+ 	<!--adding bootstrap scripts-->
+ 	
+ 	<script type="text/javascript" src="js/bootstrap.js"></script>
+ 	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+
+ 	<!--adding custom scripts--> 
+ 	<script type="text/javascript" src="js/custom.js"></script>
+
+</head>
+<body>
+<i style="font-size: 30px" class="glyphicon glyphicon-book"></i>
+
+</body>
+</html>
