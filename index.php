75f1e8fc90e8213fcce1dafe972ceb557466d086
Affan Khan <affan.ned@gmail.com>
2015-10-03 11:28:20 -0700

    second commit

--- index.php
+++ index.php
@@ -2,13 +2,15 @@
 <html>
 <head>
 
-	<title></title>
+	<title>TechShop | Technology Accessories </title>
+    <link  type="shortcut icon" rel="icon" href="images/special-deals/laptop.jpg">
 		<!--adding the stylesheets and bootstrap-->
  	
  	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
  	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
  	<link rel="stylesheet" type="text/css" href="css/font-awesome.css">
  	<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+
 
  	<!--adding custom stylesheets -->
  	<link rel="stylesheet" type="text/css" href="css/custom.css">
@@ -26,7 +28,136 @@
 
 </head>
 <body>
-<i style="font-size: 30px" class="glyphicon glyphicon-book"></i>
+<!-- navigation bar starts -->
 
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle Navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#"><i class="glyphicon glyphicon-shopping-cart"></i> TechShop.com</a>
+            </div>
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav ">
+                    <li><a href="#">Laptops</a></li>
+                    <li><a href="#">Desktops</a></li>
+                    <li><a href="#">Accessories</a></li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+
+                    <li><a href="#">Login</a></li>
+                    <li><a href="#">Register</a></li>
+
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+<!-- navigation bar ends -->
+<div class="container">
+
+    <div class="row">
+        <div class="col-md-3">
+            <div class="list-group">
+                <button type="button" class="list-group-item">Laptops</button>
+                <button type="button" class="list-group-item">Desktop</button>
+                <button type="button" class="list-group-item">Accessories</button>
+                <button type="button" class="list-group-item">Accessories</button>
+                <button type="button" class="list-group-item">Laptops</button>
+                <button type="button" class="list-group-item">Desktop</button>
+                <button type="button" class="list-group-item">Accessories</button>
+                <button type="button" class="list-group-item">Laptops</button>
+            </div>
+        </div>
+        <div class="col-md-9">
+
+            <!-- slider code starts-->
+            <div id="slideshow">
+                <div>
+                    <img src="images/banner-ads/image1.jpg">
+                </div>
+                <div>
+                    <img src="images/banner-ads/image2.jpg">
+                </div>
+                <div>
+                    <img src="images/banner-ads/image3.jpg">
+                </div>
+                <div>
+                    <img src="images/banner-ads/image4.jpg">
+                </div>
+                <div>
+                    <img src="images/banner-ads/image5.jpg">
+                </div>
+            </div>
+            <!-- slider code ends -->
+        </div>
+
+    </div>
+
+
+    <!-- special deals ads here-->
+
+    <div class="row well special-deals">
+        <div  class="col-md-4 special-deals">
+            <img class="glyphicon-align-center" src="images/special-deals/laptop.jpg">
+        </div>
+        <div class="col-md-4  special-deals">
+            <img class="glyphicon-align-center" src="images/special-deals/desktop.jpg">
+        </div>
+        <div class="col-md-4  special-deals">
+            <img class="glyphicon-align-center" src="images/special-deals/usb.jpg">
+        </div>
+    </div>
+
+
+
+    <div class="row">
+
+        <div class="col-md-12">
+
+            <div class="panel panel-default">
+                <div class="panel-heading">Laptops</div>
+                <div class="panel-body"></div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+
+        <div class="col-md-12">
+
+            <div class="panel panel-default">
+                <div class="panel-heading"> Desktops</div>
+                <div class="panel-body"></div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+
+        <div class="col-md-12">
+
+            <div class="panel panel-default">
+                <div class="panel-heading">Accessories</div>
+                <div class="panel-body">
+
+                    <!-- 3 columns-->
+                    <div class="col-md-4">
+
+                    </div>
+                    <div class="col-md-4"></div>
+                    <div class="col-md-4"></div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+</div>
 </body>
 </html>
--- js/custom.js
+++ js/custom.js
@@ -1,43 +1,17 @@
 $(document).ready(function() {
 
-	// get the data on click 
-
-$('#submit').on('click',function() {
-
-
-	// check if the word is empty
-	if($.trim($('#keyword').val()) != '') {
-
-		
-		// hide the error
-		$('.error').css('display','none');
-		
-		var search_word = $('#keyword').val();
-		console.log('hello');
-		// make an ajax request
-		var request =  $.ajax({
-			type:"get",
-			dataType:"json",
-			url: "search.php",
-			data: {word : search_word}
-		});
-
-		request.done(function(data){
-
-			$('#result-panel').css('display','block');
-			$('#word').text(data.data.word);
-			$('#word-type').text(data.data.wordtype);
-			$('#definition').text(data.data.definition);
-		});
-
-	}
-	else {
-		$('.error').css('display','inline-block').delay(2000).hide('.error');
-	}
-	
-});
-	
+    //slider control code
 
 
 
+    $("#slideshow > div:gt(0)").hide();
+
+    setInterval(function() {
+        $('#slideshow > div:first')
+            .fadeOut(1000)
+            .next()
+            .fadeIn(1000)
+            .end()
+            .appendTo('#slideshow');
+    },  5000);
 });
